08049433f0cf4f9df5b131395ae0303af5726f7d
Korotkyi-Maksym <trendy2dum@gmail.com>
2026-06-11 12:47:41 +0200

    Add files via upload

---
+++ Hry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace databaze_hry_2
+{
+    internal class Hry
+    {
+        public string Zanr { get; set; }
+        public string StavHry { get; set; }
+        public int ID { get; set; }
+        public string Nazev { get; set; }
+        private int hodnoceni;
+
+        public int Hodnoceni
+        {
+            get { return hodnoceni; }
+            set
+            {
+                if (value > 10)
+                {
+                    hodnoceni = 10;
+                }
+                else if (value < 1)
+                {
+                    hodnoceni = 1;
+                }
+                else
+                {
+                    hodnoceni = value;
+                }
+            }
+        }
+        public int Cena { get; set; }
+        
+        
+    }
+}
